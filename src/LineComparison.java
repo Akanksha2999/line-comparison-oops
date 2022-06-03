a80e3f5e8313fa58739013760b53599425d73232
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-03 21:47:14 +0530

    ADDED: Checked if length of two lines is equal or one of it is greater than other by calling the methods

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,30 +1,44 @@
-public class LineComparison {
-    public static void main(String[] args) {
+import java.lang.Math;
+import java.util.Scanner;
 
-        //Printed welcome message
-        System.out.println("Welcome to line comparison computation program");
+public class LineComparison {
+    public static double length1, length2;
+
+    public static void lineLengthCompute() {
 
         //Initialized the variables
-        int x1 = 2;
-        int x2 = 3;
-        int y1 = 3;
-        int y2 = 4;
-        int x3 = 3;
-        int x4 = 4;
-        int y3 = 2;
-        int y4 = 5;
+        double x1 = 2;
+        double x2 = 3;
+        double y1 = 3;
+        double y2 = 4;
+        double x3 = 3;
+        double x4 = 4;
+        double y3 = 2;
+        double y4 = 5;
 
         //Calculating the length of two lines
         double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         double length2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println(length1);
+        System.out.println(length2);
+    }
 
-        //Checking if both the lines are equal or one is greater than other
+    //Checking if the lines are equal or one of them is greater than the other
+    public static void checkEquality() {
         if (length1 == length2) {
-            System.out.println("lines are equal");
+            System.out.println("Both lines are equal");
         } else if (length1 > length2) {
-            System.out.println("line1 is greater than line2");
+            System.out.println("Line 1 is greater than line 2");
         } else {
-            System.out.println("line1 is less than line2");
+            System.out.println("Line 2 is greater than line 1");
         }
+    }
+
+    //Calling the methods
+    public static void main(String[] args) {
+        //Printed welcome message
+        System.out.println("Welcome to line comparison computation program");
+        lineLengthCompute();
+        checkEquality();
     }
 }
