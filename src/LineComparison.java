41c32adb4213a32ce7671a4e06d061652687e1d1
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-03 17:58:12 +0530

    ADDED: Printed welcome message

---
+++ src/LineComparison.java
@@ -0,0 +1,6 @@
+public class LineComparison {
+    public static void main(String[] args) {
+        //Printed welcome message
+        System.out.println("Welcome to line comparison computation program");
+    }
+}
