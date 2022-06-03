f431b90893af099901338e673cf4ec07d0034266
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-03 18:01:46 +0530

    ADDED: Calculated length of the line

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,6 +1,20 @@
 public class LineComparison {
     public static void main(String[] args) {
+
         //Printed welcome message
         System.out.println("Welcome to line comparison computation program");
+
+        //Initialized the variables
+        int x1 = 2;
+        int x2 = 5;
+        int y1 = 3;
+        int y2 = 1;
+
+        //Calculated length of the line
+        double length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+        //Printed length of the line
+        System.out.println("Length of the line is:" + length);
+
     }
 }
